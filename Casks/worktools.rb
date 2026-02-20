316db87149ade276f340fcb0e2ed57af352dd0fb
Khaled Alam <khaledalam.net@gmail.com>
2026-02-20 21:04:00 +0200

    update desc

--- Casks/worktools.rb
+++ Casks/worktools.rb
@@ -1,10 +1,10 @@
 cask "worktools" do
-  version "1.0.0"
-  sha256 "ab09f7db04ea7cbb86d393a0d37fe32249095c5bd2b77a82568f7902d914badd"
+  version "2.0.0"
+  sha256 "bd8be67ede9da09e428b3c247641ccb8066dd8c77dabdabeb2b11e757063ca82"
 
   url "https://github.com/khaledalam/worktools/releases/download/v#{version}/WorkTools-#{version}-arm64-mac.zip"
   name "WorkTools"
-  desc "Quick JIRA ticket opener for Mac menu bar"
+  desc "Developer productivity toolkit for Mac menu bar"
   homepage "https://khaledalam.net"
 
   app "WorkTools.app"
